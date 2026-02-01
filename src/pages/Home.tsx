34d76dee42a73d2b744a00b8579319fc18621d5d
KimHyeonSu <hirobin2001@gmail.com>
2026-02-02 01:52:06 +0900

    feat: 홈 화면 배너 UI 구현
    - shadcn/ui Carousel, embla-carousel-autoplay 설치
    - Carousel로 배너 UI 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,18 +2,24 @@ import NewMeetingList from "@/components/features/home/NewMeetingList";
 import JoinedMeetingsList from "@/components/features/home/JoinedMeetingsList";
 import PendingMeetingsList from "@/components/features/home/PendingMeetingsList";
 import HostedMeetingsList from "@/components/features/home/HostedMeetingsList";
-import SearchSection from "@features/search/SearchSection";
+// import SearchSection from "@features/search/SearchSection";
 import TopicSection from "@features/topics/TopicSection";
 import { useAuthStore } from "@store/authStore";
+import Banner from "@/components/features/home/banner";
 
 function Home() {
   const { isLoggedIn } = useAuthStore();
   return (
     <div className="flex flex-col justify-center items-center">
-      <SearchSection />
+      {/* TODO: SearchSection 삭제 */}
+      {/* <SearchSection /> */}
+      <Banner />
       <div className="flex flex-col pt-8 items-center w-full">
+        {/* TODO: TopicSection 삭제 후 카테고리 섹션 추가 */}
         <TopicSection />
+        {/* TODO: LikeMeetingList(좋아요순), 특정 카테고리 몇 개 추가 */}
         <NewMeetingList />
+        {/* TODO: 후기 리스트 추가 */}
         {isLoggedIn && (
           <>
             <JoinedMeetingsList />
@@ -21,7 +27,6 @@ function Home() {
             <PendingMeetingsList />
           </>
         )}
-
       </div>
     </div>
   );
