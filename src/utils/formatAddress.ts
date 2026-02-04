b64addb768239f952657d3c48b269ee41a24fa0e
KimHyeonSu <hirobin2001@gmail.com>
2026-02-05 00:49:39 +0900

    feat: 주소 표시 로직 변경

--- src/utils/formatAddress.ts
+++ src/utils/formatAddress.ts
@@ -1,22 +1,47 @@
-// 시/군/구 주소만 보여주는 함수
-export const getDistrictFromAddress = (fullAddress: string): string => {
+// 주소에서 표시할 부분을 추출하는 함수
+export const getDisplayAddress = (fullAddress: string): string => {
   if (!fullAddress) return "";
 
-  // 1. 공백으로 주소 쪼개기
   const parts = fullAddress.split(" ");
+  let firstLevel = ""; // ex) 서울특별시, 경기도
+  let secondLevel = ""; // ex) 강남구, 가평군
 
-  // 2. '구'로 끝날 때(동작구, 분당구)
-  const gu = parts.find((part) => part.endsWith("구"));
-  if (gu) return gu;
+  // 1. 시/도
+  for (const part of parts) {
+    if (
+      part.endsWith("특별시") ||
+      part.endsWith("광역시") ||
+      part.endsWith("특별자치시") ||
+      part.endsWith("도") ||
+      part.endsWith("시")
+    ) {
+      firstLevel = part;
+      break;
+    }
+  }
 
-  // 3. '군'으로 끝날 때(가평군, 태안군)
-  const gun = parts.find((part) => part.endsWith("군"));
-  if (gun) return gun;
+  // 2. 구/군
+  const firstLevelIndex = firstLevel ? parts.indexOf(firstLevel) : -1;
+  const startIndexForSecondLevel =
+    firstLevelIndex !== -1 ? firstLevelIndex + 1 : 0;
 
-  // 4. '시'로 끝내기(서울시, 세종시)
-  const si = parts.find((part) => part.endsWith("시"));
-  if (si) return si;
+  for (let i = startIndexForSecondLevel; i < parts.length; i++) {
+    const part = parts[i];
+    if (part.endsWith("구") || part.endsWith("군")) {
+      secondLevel = part;
+      break;
+    }
+  }
 
-  // 5. 모두 충족되지 않을 때 그냥 첫 번째 단어 반환
-  return parts[0];
+  // 3. 결합 후 return
+  if (firstLevel && secondLevel) {
+    return `${firstLevel} ${secondLevel}`;
+  } else if (firstLevel) {
+    return firstLevel;
+  } else if (secondLevel) {
+    // 만약 주소가 '구'나 '군'으로 바로 시작하는 경우
+    return secondLevel;
+  } else {
+    return parts[0] || ""; // 일치하는 부분이 없으면 첫 번째 단어 반환
+  }
 };
