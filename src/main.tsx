4ee9000b17ff261188ae2f18e43d45250678aa00
in7570 <i7570@naver.com>
2026-02-10 17:31:57 +0900

    refac: queryClient defaultOptions 등록
    - staleTime 5분 뒤에 상한 상태로 두기
    - gcTime 10분간 데이터 보관
    - retry 1회로 제한하여 사용자 기다리는 시간 줄이기
    - refetchOnWindowFocus false로 api 요청 줄여서 네트워크 절약

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "./lib/queryClient.ts";
+import { QueryClient } from '@tanstack/react-query'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 // import "pretendard/dist/web/static/pretendard.css";
 
@@ -20,6 +20,17 @@ async function enableMocking() {
     onUnhandledRequest: "bypass",
   });
 }
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      gcTime: 1000 * 60 * 10,
+      retry: 1,
+      refetchOnWindowFocus: false,
+    }
+  }
+})
 
 enableMocking().then(() => {
   const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
