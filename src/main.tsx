cb001d56242fb2b160aa2ef39e34aa00b36babbc
in7570 <i7570@naver.com>
2026-02-12 01:11:41 +0900

    refac: queryclient default options 위치 변경

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { QueryClient } from '@tanstack/react-query'
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { queryClient } from "./lib/queryClient";
 // import "pretendard/dist/web/static/pretendard.css";
 
 async function enableMocking() {
@@ -20,17 +20,6 @@ async function enableMocking() {
     onUnhandledRequest: "bypass",
   });
 }
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60 * 5,
-      gcTime: 1000 * 60 * 10,
-      retry: 1,
-      refetchOnWindowFocus: false,
-    }
-  }
-})
 
 enableMocking().then(() => {
   const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
